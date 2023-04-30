6f7d336e1678b6a17932a6e01a08b1da7525e0a7
Atrem Petrenko <64548375+Artem-ka-create@users.noreply.github.com>
2023-04-30 19:12:35 +0200

    Update HeaderComponent.js

--- src/components/Header/HeaderComponent.js
+++ src/components/Header/HeaderComponent.js
@@ -6,7 +6,7 @@ function HeaderComponent({headerText}){
 
 return(
     <h1 className={styles.headerComponent} >
-    <img style={{ width:50, marginRight:50}} src='T-logo.png'/>
+//     <img style={{ width:50, marginRight:50}} src='T-logo.png'/>
     {headerText}
     </h1>
 )
